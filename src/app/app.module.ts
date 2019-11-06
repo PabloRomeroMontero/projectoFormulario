65c051269944d2a65b15fe5dc0466bd393f8fe10
PabloRomeroMontero <pabloromeromontero23@gmail.com>
2019-11-06 12:43:29 +0100

    Plagado de errores

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,27 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HomeComponent } from './components/home/home.component';
-import { FormComponent } from './components/form/form.component';
-import {APP_ROUTING} from './components/app.route';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {LogComponent} from './components/log/log.component';
+import {FormComponent} from './components/form/form.component';
+import {APP_ROUTES, APP_ROUTING} from './app.route';
 import {FormsModule} from '@angular/forms';
+import {RouterModule} from '@angular/router';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
+    LogComponent,
     FormComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    APP_ROUTING,
     FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- src/app/app.route.ts
+++ src/app/app.route.ts
@@ -1,24 +1,12 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {RouterModule, Routes} from '@angular/router';
 import {LogComponent} from './components/log/log.component';
 import {FormComponent} from './components/form/form.component';
 
-
-
-const APP_ROUTES: Routes = [
+export const APP_ROUTES: Routes = [
   { path: 'log', component: LogComponent},
   { path: 'form', component: FormComponent},
   { path: '**', component: FormComponent}
 ];
 
 
-@NgModule({
-  imports: [RouterModule.forRoot(APP_ROUTES)],
-  exports: [RouterModule]
-})
-
-
-
-
-
-export class AppRoutingModule { }
+export const APP_ROUTING = RouterModule.forRoot(APP_ROUTES);
